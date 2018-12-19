af4556377f39f10261844bb6fbe29b19ff091c47
Henry <hanke.liu@gmail.com>
2018-12-19 15:38:40 -0500

    tested cli.rb also works when executible file news_reader_cli ran

--- lib/news_reader_cli.rb
+++ lib/news_reader_cli.rb
@@ -1,6 +1,6 @@
 require "pry"
 require_relative "./news_reader_cli/version" #require "news_reader_cli/version" #
-#require_relative "./news_reader_cli/cli"
+require_relative "./news_reader_cli/cli"
 
 module NewsReaderCli
   # Your code goes here...
--- lib/news_reader_cli/cli.rb
+++ lib/news_reader_cli/cli.rb
@@ -1,7 +1,8 @@
-class NewsReaderCli::Cli
-
-  def call
-    puts "cli call is working"
-  end
-  
-end
+puts "cli is also working"
+# class NewsReaderCli::Cli
+#
+#   def call
+#     puts "cli call is working"
+#   end
+#
+# end
