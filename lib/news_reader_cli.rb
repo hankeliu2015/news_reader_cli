37080dcb28d648cea4a664000121885462033785
Henry <hanke.liu@gmail.com>
2018-12-19 21:48:55 -0500

    created news_api.rb to parse api and updated news_reader_cli.rb with require open_uri and require_relative api_service

--- lib/news_reader_cli.rb
+++ lib/news_reader_cli.rb
@@ -1,6 +1,8 @@
 require "pry"
+require "open-uri"
 require_relative "./news_reader_cli/version" #require "news_reader_cli/version" #
 require_relative "./news_reader_cli/cli"
+require_relative "./news_reader_cli/api_service"
 
 module NewsReaderCli
   # Your code goes here...
