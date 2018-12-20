4601f6c48f31e63b97c1f5808e01025aa24614dd
Henry <hanke.liu@gmail.com>
2018-12-19 22:50:44 -0500

    set api pageSize to max

--- lib/news_reader_cli/api_service.rb
+++ lib/news_reader_cli/api_service.rb
@@ -2,16 +2,16 @@ class NewsReaderCli::ApiService
 
   def self.newsapi
 
-    url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=71fa5ddcc82d4f8ab52693f5f0ad3784"
-    # url = 'https://newsapi.org/v2/top-headlines?'\
-    #       'country=us&'\
-    #       'apiKey=71fa5ddcc82d4f8ab52693f5f0ad3784'\
-    #       'pageSize=100'
+    #url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=71fa5ddcc82d4f8ab52693f5f0ad3784&pageSize=100"
+    url = 'https://newsapi.org/v2/top-headlines?'\
+          'country=us&'\
+          'apiKey=71fa5ddcc82d4f8ab52693f5f0ad3784&'\
+          'pageSize=100'
     req = open(url)
     response_body = req.read
     json = JSON.parse(response_body)
     json["articles"]
-    #binding.pry
+    binding.pry
   end
 
 end
