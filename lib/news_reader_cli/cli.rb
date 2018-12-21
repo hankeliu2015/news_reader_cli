c9d82ca8411b2089932237f2a2eb1914cbfaaead
Henry <hanke.liu@gmail.com>
2018-12-21 00:01:53 -0500

    added loop to #menu in cli.rb. but elsif condition still not working

--- lib/news_reader_cli/cli.rb
+++ lib/news_reader_cli/cli.rb
@@ -8,27 +8,58 @@ class NewsReaderCli::CLI
   end
 
   def menu
-    puts "Here are today's news headlines"
-    NewsReaderCli::Article.list_all_titles
 
-    puts "To read an article, please type a index number. To Exit, type exit. "
+    puts "Please type 'l' today's news headlines."
+    puts "To read an article, please type 'a' follow by index number."
+    puts "To get an article url, please type 'u' follow by index number."
+    puts "To Exit, type 'exit'."
 
     input = ""
+
     while input != "exit" do
       input = gets.chomp.strip
 
-      if (1..(NewsReaderCli::Article.all.length)).include?(input.to_i)
-
-        found_url = NewsReaderCli::Article.find_url_by_article_index(input.to_i)
+      if input == "l"
+        NewsReaderCli::Article.list_all_titles
+        #binding.pry
+      elsif input.slice(0) == "a" && (1..(NewsReaderCli::Article.all.length)).include?(input.slice(0).to_i)
+        found_content = NewsReaderCli::Article.find_content_by_article_index(input.to_i)
+        puts "#{found_content}"
+        #binding.pry
+      elsif input.slice(0) == "u" && (1..(NewsReaderCli::Article.all.length)).include?(input.slice(0).to_i)
+        found_url = NewsReaderCli::Article.find_url_by_article_index(input.slice(0).to_i)
         puts "#{found_url}"
-
-        # found_content = NewsReaderCli::Article.find_content_by_article_index(input.to_i)
-        # puts "#{found_content}"
+        # binding.pry
+        # puts "0"
       else
-        puts "It is not a valid input article number, please re-type the number match artiles."
+        puts "Please enter a valid input article number, please follow instructions."
+        puts "Please type 'l' today's news headlines."
+        puts "To read an article, please type 'a' follow by index number."
+        puts "To get an article url, please type 'u' follow by index number."
+        puts "To Exit, type 'exit'."
+
       end
     end
-
   end
+end #end of class
 
-end # end of class
+        # input2 = ""
+        # input2 = gets.chomp.strip
+        #
+        #   if  input2.slice!(0) == "a" && (1..(NewsReaderCli::Article.all.length)).include?(input2.to_i)
+        #     found_content = NewsReaderCli::Article.find_content_by_article_index(input2.to_i)
+        #     puts "#{found_content}"
+        #     puts "Please type in 'l' to get today's news headlines"
+        #     puts "To Exit, pease type 'exit'."
+        #
+        #   elsif input2.slice!(0) == "u" && (1..(NewsReaderCli::Article.all.length)).include?(input2.to_i)
+        #     found_url = NewsReaderCli::Article.find_url_by_article_index(input2.to_i)
+        #     puts "#{found_url}"
+        #     puts "Please type in 'l' to get today's news headlines"
+        #     puts "To Exit, pease type 'exit'."
+        #   elsif
+        #     puts "It is not a valid input article number, please follow instructions."
+        #   end
+      # else
+      #   puts "Please type 'l' today's news headlines."
+      #   puts "To Exit, type 'exit'."
