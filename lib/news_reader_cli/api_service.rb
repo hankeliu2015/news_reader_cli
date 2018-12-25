169683dc406f4f602cf2df71f908cfd2edb1f749
Henry <hanke.liu@gmail.com>
2018-12-25 00:07:38 -0500

    remove the apikey from api_service.rb

--- lib/news_reader_cli/api_service.rb
+++ lib/news_reader_cli/api_service.rb
@@ -1,7 +1,7 @@
 class NewsReaderCli::ApiService
 
   def self.newsapi
-    #url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=71fa5ddcc82d4f8ab52693f5f0ad3784&pageSize=100"
+    #url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=(put key here)&pageSize=100"
     assigned_api_key = ENV['key'].strip
     #binding.pry
     url = 'https://newsapi.org/v2/top-headlines?'\
