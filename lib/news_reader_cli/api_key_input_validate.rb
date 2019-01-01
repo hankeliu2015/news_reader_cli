3e276d0622dff570945c708c9ac42806bd4b1f6f
Henry <hanke.liu@gmail.com>
2019-01-01 12:46:46 -0500

    version 3 for Rubygems.org.

--- lib/news_reader_cli/api_key_input_validate.rb
+++ lib/news_reader_cli/api_key_input_validate.rb
@@ -1,28 +1,30 @@
 class NewsReaderCli::ApiKeyInputValidate
 
-  def key_input_validate
-    puts "please type in API Key from API News:"
-    @key = gets.chomp.strip
-
-    while !key.match(/([0-9]|\w){32}/)  do
-
-      break if @key == "exit"
-      # need to get out of the loop and remove the file .env
-
-      puts <<-HEREDOC
-
-      Your API keys value might missing digits or contain invalid Charactors.
-      Please verify your API Key and try again. Thanks!
-
-      HEREDOC
-
-      @key = gets.chomp.strip
-    end
-
-  end #end of method
-
-  def key
-    @key
-  end
+  # def key_input_validate
+  #   puts "please type in API Key from API News:"
+  #
+  #   @key = gets.chomp.strip
+  #
+  #   while !@key.match(/([0-9]|\w){32}/)  do
+  #
+  #     break if @key == "exit"
+  #     # need to get out of the loop and remove the file .env
+  #
+  #     puts <<-HEREDOC
+  #
+  #     Your API keys value might missing digits or contain invalid Charactors.
+  #     Please verify your API Key and try again. Thanks!
+  #
+  #     HEREDOC
+  #
+  #     @key = gets.chomp.strip
+  #   end
+  #
+  # end #end of method
+  #
+  #
+  # def validated_key
+  #   @key
+  # end
 
 end #end of class
